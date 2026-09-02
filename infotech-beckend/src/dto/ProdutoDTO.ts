d7f74792d0b564b44bb72139f67e0d6243f58098
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-09-02 09:48:32 -0300

    feat: Criando dto Produto

---
+++ infotech-beckend/src/dto/ProdutoDTO.ts
@@ -0,0 +1,12 @@
+export default interface ProdutoDTO {
+    id_produto: number;
+    id_categoria: number;
+    codigo: string;
+    nome: string;
+    descricao: string;
+    preco_unitario: number;
+    quantidade_disponivel: number;
+    quantidade_minima: number;
+    ativo: boolean;
+    data_cadastro: string;
+}

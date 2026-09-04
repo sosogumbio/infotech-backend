18b723197f35c9c19669de7741171d78db91643b
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-09-04 11:21:59 -0300

    feat: Atualizando server.ts

--- infotech-beckend/src/server.ts
+++ infotech-beckend/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(router);
+app.use("/api", router);
 
 app.listen(3333, () => {
     console.log("Servidor rodando na porta 3333");
